62ced4d8992636963050b01ebb28ee93bfd1ec73
Jukem <chris@dixn.dev>
2025-01-23 07:33:58 -0700

    homework 2 completed

---
+++ Card.java
@@ -0,0 +1,13 @@
+public class Card
+{
+   String rank = "J";
+   String suit = "Hearts";
+
+   public static void main(String[] args){
+     Card card = new Card();
+     System.out.println(card.rank);
+     System.out.println(card.suit);
+   }
+}
+
+
