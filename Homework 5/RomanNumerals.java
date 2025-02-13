579f81913309ea37f81f1228d9d5df0feef950ef
Jukem <chris@dixn.dev>
2025-02-13 09:39:17 -0700

    homework 6 done

--- Homework 5/RomanNumerals.java
+++ Homework 5/RomanNumerals.java
@@ -7,7 +7,7 @@ public class RomanNumerals {
         Scanner scanner = new Scanner(System.in);
         System.out.print("\nEnter a number between 1 and 10 (inclusive): ");
         int number = scanner.nextInt();
-        String[] romanNumerals ={"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        String[] romanNumerals = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         if(number < 1 || number > 10) System.out.println("Invalid number");
         else System.out.println("Roman numeral: " + romanNumerals[number - 1]);
         scanner.close();
---
+++ Homework 6/SwitchRoulette.java
@@ -0,0 +1,123 @@
+// Chris Dixon
+import.java.util.Scanner;
+
+public class SwitchRoulette {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        switch(number) {
+            case 1:
+                System.out.println("red");
+                break;
+            case 2:
+                System.out.println("black");
+                break;
+            case 3:
+                System.out.println("red");
+                break;
+            case 4:
+                System.out.println("black");
+                break;
+            case 5:
+                System.out.println("red");
+                break;
+            case 6:
+                System.out.println("black");
+                break;
+            case 7:
+                System.out.println("red");
+                break;
+            case 8:
+                System.out.println("black");
+                break;
+            case 9:
+                System.out.println("red");
+                break;
+            case 10:
+                System.out.println("black");
+                break;
+            case 11:
+                System.out.println("black");
+                break;
+            case 12:
+                System.out.println("red");
+                break;
+            case 13:
+                System.out.println("black");
+                break;
+            case 14:
+                System.out.println("red");
+                break;
+            case 15:
+                System.out.println("black");
+                break;
+            case 16:
+                System.out.println("red");
+                break;
+            case 17:
+                System.out.println("black");
+                break;
+            case 18:
+                System.out.println("red");
+                break;
+            case 19:
+                System.out.println("black");
+                break;
+            case 20:
+                System.out.println("red");
+                break;
+            case 21:
+                System.out.println("red");
+                break;
+            case 22:
+                System.out.println("black");
+                break;
+            case 23:
+                System.out.println("red");
+                break;
+            case 24:
+                System.out.println("black");
+                break;
+            case 25:
+                System.out.println("red");
+                break;
+            case 26:
+                System.out.println("black");
+                break;
+            case 27:
+                System.out.println("red");
+                break;
+            case 28:
+                System.out.println("black");
+                break;
+            case 29:
+                System.out.println("red");
+                break;
+            case 30:
+                System.out.println("red");
+                break;
+            case 31:
+                System.out.println("black");
+                break;
+            case 32:
+                System.out.println("red");
+                break;
+            case 33:
+                System.out.println("black");
+                break;
+            case 34:
+                System.out.println("red");
+                break;
+            case 35:
+                System.out.println("black");
+                break;
+            case 36:
+                System.out.println("red");
+                break;
+            default:
+                System.out.println("greem");
+            
+        }
+        input.close();
+    }
+}
