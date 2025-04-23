edbdb2e692d2f3c289a82909a49b83bfd0ab258a
Jukem <chris@dixn.dev>
2025-04-22 21:39:51 -0700

    fixed folders

--- Homework 1/MyFirstProgram.java
+++
@@ -1,9 +0,0 @@
-// This is my first Java program.
-public class MyFirstProgram
-{
-   public static void main(String[] args)
-   {
-      System.out.println();                   //start on a new line
-      System.out.println("Hello World!");
-   }
-}
